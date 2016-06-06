fb800a103795f23b8ce0787285d14a8b944592c4
cecilelebon <cecile.anna.lebon@gmail.com>
2016-06-06 18:58:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+$(".helloworld").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".hello").offset().top
+    }, 1000);
+});
+
+$(".photography").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".photo").offset().top
+    }, 1000);
+});
+
+$(".graphicdesign").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".graphic").offset().top
+    }, 1000);
+});
+
+
+$(".motiongraphics").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".motion").offset().top
+    }, 1000);
+});
+
+
+$(".motiongraphics").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".motion").offset().top
+    }, 1000);
+});
